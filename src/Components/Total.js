4d401cae6fc8cd477751bb2e2265e15fc48c7ff1
Gerardo <gerardmoe@hotmail.com>
2022-11-13 18:47:22 -0500

    bug volumen total

--- src/Components/Total.js
+++ src/Components/Total.js
@@ -5,7 +5,7 @@ export const Total = ({ vigas }) => {
     <div className="card bg-light col-lg-2 mx-3">
       <div className="card-body text-black">
         <h5 className="card-title">Cantidades totales:</h5>
-        <p className="text-nowrap"><small>Concreto: {volt} m³</small></p>
+        <p className="text-nowrap"><small>Concreto: {volt.toFixed(2)} m³</small></p>
       </div>
     </div>
   );
